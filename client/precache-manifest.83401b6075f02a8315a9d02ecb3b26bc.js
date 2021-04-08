f78c2d3bd9037802642868cdb989bc492d045fdf
Paul Beccio <pjbeccio@mac.com>
2021-04-08 10:39:25 -0400

    Packaging for initial Heroku deployment

---
+++ client/precache-manifest.83401b6075f02a8315a9d02ecb3b26bc.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a813003077053971d2b3837c67ec9e8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "843237fb114a20393974",
+    "url": "/static/css/main.9c6a1fc2.chunk.css"
+  },
+  {
+    "revision": "854d16484b597077689d",
+    "url": "/static/js/2.199dcee5.chunk.js"
+  },
+  {
+    "revision": "f2a34901425dfca8a85ec532caa45ac1",
+    "url": "/static/js/2.199dcee5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "843237fb114a20393974",
+    "url": "/static/js/main.ae337d2c.chunk.js"
+  },
+  {
+    "revision": "73f94224884e0ba06838",
+    "url": "/static/js/runtime-main.df8613b9.js"
+  },
+  {
+    "revision": "1f08df83901a1757a57b00f98046c990",
+    "url": "/static/media/action-tick-checked.1f08df83.svg"
+  },
+  {
+    "revision": "0e3156db5550f8cf804ca123f421a710",
+    "url": "/static/media/action-tick-unchecked.0e3156db.svg"
+  },
+  {
+    "revision": "e3a01ea7bd909dfefb4d34438763ff07",
+    "url": "/static/media/face-dead.e3a01ea7.svg"
+  },
+  {
+    "revision": "e2980c5fa72a235a045a3ad9b68f4ea2",
+    "url": "/static/media/happy.e2980c5f.svg"
+  },
+  {
+    "revision": "ae90c426d47e13d950b8da7fa4c42dcf",
+    "url": "/static/media/icon-eye.ae90c426.svg"
+  },
+  {
+    "revision": "4788264df1e6ebafb9212effd2e86cb8",
+    "url": "/static/media/icon-locked.4788264d.svg"
+  },
+  {
+    "revision": "fc3d8f6f67b5a89a385409b64a050598",
+    "url": "/static/media/meh.fc3d8f6f.svg"
+  },
+  {
+    "revision": "6c7c73055a382fa780ce47365278ca77",
+    "url": "/static/media/sad.6c7c7305.svg"
+  }
+]);
